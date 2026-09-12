edef46d19f1f216967a1133b40ecab9b2758c03b
Joachim Eriksson <sa0azy@gmail.com>
2026-09-12 18:54:32 +0200

    Bump runtime version to 0.1.0-beta.1

--- custom_components/device_maintenance/const.py
+++ custom_components/device_maintenance/const.py
@@ -6,7 +6,7 @@ from homeassistant.const import Platform
 
 DOMAIN = "device_maintenance"
 NAME = "Device Maintenance"
-VERSION = "0.1.0-dev.2"
+VERSION = "0.1.0-beta.1"
 
 PLATFORMS: list[Platform] = [Platform.SENSOR, Platform.BUTTON]
 
