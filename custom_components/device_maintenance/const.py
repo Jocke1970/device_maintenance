57ca7294d8cd2f02945d6fef95b66684e1ea92f1
Joachim Eriksson <sa0azy@gmail.com>
2026-09-13 18:21:54 +0200

    Bump development version to dev.7

--- custom_components/device_maintenance/const.py
+++ custom_components/device_maintenance/const.py
@@ -6,7 +6,7 @@ from homeassistant.const import Platform
 
 DOMAIN = "device_maintenance"
 NAME = "Device Maintenance"
-VERSION = "0.1.0-dev.6"
+VERSION = "0.1.0-dev.7"
 
 PLATFORMS: list[Platform] = [Platform.SENSOR, Platform.BUTTON]
 
